2ec4ef88b2c74fb6d6ba0de3a120135aa903bf17
Esdras Eduardo <esdrasedu@gmail.com>
2018-07-10 17:58:22 -0300

    change version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NervesHcsr04.MixProject do
   def project do
     [
       app: :nerves_hcsr04,
-      version: "0.1.0",
+      version: "0.1.1",
       compilers: [:elixir_make] ++ Mix.compilers(),
       description: description(),
       package: package(),
